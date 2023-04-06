132175709e3d4350c2ee8a85c150ce641542bb16
ClaudioCarige <ccarige@gmail.com>
2023-04-06 15:10:04 -0300

    Created FinByCNPJ

--- src/main/java/com/claudiocarige/CntrMovimentApi/repositories/ClientRepository.java
+++ src/main/java/com/claudiocarige/CntrMovimentApi/repositories/ClientRepository.java
@@ -1,9 +1,13 @@
 package com.claudiocarige.CntrMovimentApi.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.claudiocarige.CntrMovimentApi.domain.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Long>{
+
+	Optional<Client> findByCnpj(String cnpj);
 
 }
