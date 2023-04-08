3a39b9d0bd49ef9b59aa269b764e08b85daebd02
ClaudioCarige <ccarige@gmail.com>
2023-04-08 09:43:03 -0300

    Created Insert Container

--- src/main/java/com/claudiocarige/CntrMovimentApi/resources/ContainerResource.java
+++ src/main/java/com/claudiocarige/CntrMovimentApi/resources/ContainerResource.java
@@ -1,16 +1,24 @@
 package com.claudiocarige.CntrMovimentApi.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.claudiocarige.CntrMovimentApi.domain.Container;
+import com.claudiocarige.CntrMovimentApi.domain.dtos.ContainerDTO;
 import com.claudiocarige.CntrMovimentApi.services.ContainerService;
+import com.claudiocarige.CntrMovimentApi.services.exception.DataIntegrityViolationException;
 
 @RestController
 @RequestMapping(value = "/api/cntr")
@@ -29,5 +37,16 @@ public class ContainerResource {
 	public ResponseEntity<Container> findById(@PathVariable Long id){
 		Container cntr = cntrService.findById(id);
 	    return ResponseEntity.ok().body(cntr);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ContainerDTO> insert(@Valid @RequestBody ContainerDTO cntrDTO){
+		cntrDTO.setCntrNumber(cntrDTO.getCntrNumber().replaceAll("[^a-zA-Z0-9]+", ""));
+	 	if (!cntrDTO.getCntrNumber().matches("[A-Z]{4}[0-9]{7}")) {
+			throw new DataIntegrityViolationException("Numero do CNTR deve conter 4 letras MAIÚSCULAS e 7 Numeros!");
+		}
+	 	Container newCntr = cntrService.insert(cntrDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(newCntr.getId()).toUri();
+	 	return ResponseEntity.created(uri).build();
 	}
  }
--- src/main/java/com/claudiocarige/CntrMovimentApi/services/ContainerService.java
+++ src/main/java/com/claudiocarige/CntrMovimentApi/services/ContainerService.java
@@ -6,22 +6,56 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.claudiocarige.CntrMovimentApi.domain.Client;
 import com.claudiocarige.CntrMovimentApi.domain.Container;
+import com.claudiocarige.CntrMovimentApi.domain.dtos.ContainerDTO;
+import com.claudiocarige.CntrMovimentApi.repositories.ClientRepository;
 import com.claudiocarige.CntrMovimentApi.repositories.ContainerRepository;
+import com.claudiocarige.CntrMovimentApi.services.exception.DataIntegrityViolationException;
+import com.claudiocarige.CntrMovimentApi.services.exception.NoSuchElementException;
 
 @Service
 public class ContainerService {
 
-	
 	@Autowired
 	private ContainerRepository cntrRepository;
 	
-	public List<Container> findAll(){
+	@Autowired
+	private ClientRepository clientRepository;
+
+	public List<Container> findAll() {
 		return cntrRepository.findAll();
 	}
-	
+
 	public Container findById(Long id) {
 		Optional<Container> cntr = cntrRepository.findById(id);
-		return cntr.orElseThrow();
+		return cntr.orElseThrow(() -> new NoSuchElementException("Container não encontrado!"));
+	}
+ 
+	public Container insert(ContainerDTO cntrDTO) {
+		cntrDTO.setId(null);
+		validateCntr(cntrDTO.getCntrNumber());
+		Optional<Client> client = clientRepository.findById(cntrDTO.getClient().getId());
+		Container newCntr = transformDTO(cntrDTO);
+		newCntr.setClient(client.get());
+		return cntrRepository.save(newCntr);
+	}
+  
+	public void validateCntr(String cntrNumber) {
+		Optional<Container> cntr = cntrRepository.findByCntrNumber(cntrNumber);
+		if (!cntr.isEmpty() && cntr.get().getCntrNumber().equals(cntrNumber)) {
+			throw new DataIntegrityViolationException("Container já existe na base de dados!");
+		}
+	}
+
+	public Container transformDTO(ContainerDTO cntrDTO) {
+		Container cntr = new Container();
+		cntr.setId(cntrDTO.getId());
+		cntr.setCntrNumber(cntrDTO.getCntrNumber());
+		cntr.setStatusCntr(cntrDTO.getStatusCntr());
+		cntr.setTypeCntr(cntrDTO.getTypeCntr());
+		cntr.setCategoryCntr(cntrDTO.getCategoryCntr());
+		cntr.setClient(cntrDTO.getClient());
+		return cntr;
 	}
 }
